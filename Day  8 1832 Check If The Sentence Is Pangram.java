492b746a999d31f560601c9e3cea92ff22521907
fouzia07-ux <fouzia07m@gmail.com>
2026-07-21 11:25:10 +0530

    Create Day  8 1832 Check If The Sentence Is Pangram.java

---
+++ Day  8 1832 Check If The Sentence Is Pangram.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (sentence.indexOf(c) == -1)
+                return false;
+        }
+        return true;
+    }
+}
